21c6ba5429d87c1fb244ca4616db93a0b5e2c7ba
Simon-Libert <91894559+Simon-Libert@users.noreply.github.com>
2022-08-28 11:30:55 +0200

    📦 NEW: init

--- src/app/tab1/tab1.module.ts
+++ src/app/tab1/tab1.module.ts
@@ -2,6 +2,7 @@ import { IonicModule } from '@ionic/angular';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { Tab1Page } from './tab1.page';
 import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
 
@@ -12,9 +13,10 @@ import { Tab1PageRoutingModule } from './tab1-routing.module';
     IonicModule,
     CommonModule,
     FormsModule,
+    ReactiveFormsModule,
     ExploreContainerComponentModule,
     Tab1PageRoutingModule
   ],
   declarations: [Tab1Page]
 })
-export class Tab1PageModule {}
+export class Tab1PageModule { }
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms'
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {
+export class Tab1Page implements OnInit {
+  form: FormGroup; // création d'un membre form à l'extérieur pour pouvoir y accéder depuis plusieurs méthodes
 
-  constructor() {}
+  constructor() { }
+
+  ngOnInit() {
+    this.form = new FormGroup({ //new instance de formGroup, on y passe un objet de config
+      foodName: new FormControl(null, {
+        validators: [Validators.required]
+      }), //un form group peut contenir plusieurs form control
+      datePlacedInFreezer: new FormControl(null, {
+        validators: [Validators.required]
+      })
+    })
+  }
+
+  add() {
+    console.log(this.form);
+
+  }
 
 }
